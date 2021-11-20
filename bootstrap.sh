f1b2aa4ed5e890323c457781070b8f15f6298bcd
mehlj <justenmehl12@gmail.com>
2021-11-20 06:09:47 -0800

    Add ssh key to ansible cli calls

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,20 +18,20 @@ done
 ls -tlar /root/
 
 # Bootstrap cluster with kubespray
-ansible-playbook -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml kubespray/cluster.yml -b 
+ansible-playbook -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml kubespray/cluster.yml -b --private-key /home/runner/.ssh/github_actions
 
 # Enable SELinux
-ansible all -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m selinux -a "policy=targeted state=enforcing" -b
+ansible all -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m selinux -a "policy=targeted state=enforcing" -b --private-key /home/runner/.ssh/github_actions
 
 # Change kubelet.env SELinux context to resolve inital issue
-ansible all -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m shell -a "semanage fcontext -a -t etc_t -f f /etc/kubernetes/kubelet.env; restorecon /etc/kubernetes/kubelet.env" -b
+ansible all -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m shell -a "semanage fcontext -a -t etc_t -f f /etc/kubernetes/kubelet.env; restorecon /etc/kubernetes/kubelet.env" -b --private-key /home/runner/.ssh/github_actions
 
 # Reboot all nodes and wait for them to come back up
-ansible all -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m reboot -b 
+ansible all -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m reboot -b --private-key /home/runner/.ssh/github_actions
 
 # Allow non-credentialed use of kubectl (only on control plane hosts)
-ansible kube_control_plane -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m file -a "path=/home/mehlj/.kube/ owner=mehlj group=mehlj state=directory" -u mehlj
-ansible kube_control_plane -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m copy -a "src=/etc/kubernetes/admin.conf dest=/home/mehlj/.kube/config owner=mehlj group=mehlj remote_src=yes mode=0600" -u mehlj -b
+ansible kube_control_plane -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m file -a "path=/home/mehlj/.kube/ owner=mehlj group=mehlj state=directory" -u mehlj --private-key /home/runner/.ssh/github_actions
+ansible kube_control_plane -i kubespray/inventory/$kubespray_cluster_name/hosts.yaml -m copy -a "src=/etc/kubernetes/admin.conf dest=/home/mehlj/.kube/config owner=mehlj group=mehlj remote_src=yes mode=0600" -u mehlj -b --private-key /home/runner/.ssh/github_actions
 
 # Deploy traefik and example hello-world applications
-ansible-playbook ansible/playbooks/traefik.yml -b --vault-password-file $vault_password_file
+ansible-playbook ansible/playbooks/traefik.yml -b --vault-password-file $vault_password_file --private-key /home/runner/.ssh/github_actions
